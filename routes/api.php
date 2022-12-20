e89352fa2cbfe7c473059d88cebb372fdc94f39e
Anderson1405 <anderson.cardona@evertecinc.com>
2022-12-19 23:08:14 -0500

    feat: add the controller, resource and route for the api register

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\StudentApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('api-psicol', StudentApiController::class);
 
 
